3ef093869cf7ff7ff5dc97e012d5eca0850a1cac
Arham Affan <arhamaffan22@gmail.com>
2025-07-07 15:09:43 +0500

    05: AI Jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,33 @@
 "use client";
 
 import { toast } from "sonner";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { Input } from "@/components/ui/input";
 
 const Page = () => {
+  const [value, setValue] = useState("");
 
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({
-    onSuccess: ()=>{
-      toast.success("Background job started");
-    }
-  }));
-  return <div className="p-4 max-w-7xl mx-auto">
-    <Button disabled={invoke.isPending} onClick={() => invoke.mutate({text: "John"})}>Invoke Background Job</Button>
-  </div>;
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Background job started");
+      },
+    })
+  );
+  return (
+    <div className="p-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button
+        disabled={invoke.isPending}
+        onClick={() => invoke.mutate({ value: value })}
+      >
+        Invoke Background Job
+      </Button>
+    </div>
+  );
 };
 export default Page;

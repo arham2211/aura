a87304b0410a47c9f42e34a9a3ad202fdb63af4a
Arham Affan <arhamaffan22@gmail.com>
2025-07-07 21:57:47 +0500

    06: e2b sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,21 +1,32 @@
+import { Sandbox } from "@e2b/code-interpreter";
 import { openai, createAgent } from "@inngest/agent-kit";
+import { getSandbox } from "./utils";
 
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("aura-farming-dev");
+      return sandbox.sandboxId;
+    });
     const codeAgent = createAgent({
       name: "code-agent",
       system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
-      model: openai({ model: "gpt-4o" }),
+      model: openai({ model: "gpt-4o-mini" }),
     });
 
     const { output } = await codeAgent.run(
       `Write the following snippet: ${event.data.value}`
     );
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
 
-    return { output };
+    return { output, sandboxUrl };
   }
 );
